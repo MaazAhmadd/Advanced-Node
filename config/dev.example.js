771ff622c6fd47d400fa5a585e5ea7bb59e14f39
MaazAhmadd <maaz.ahmad213@gmail.com>
2022-09-08 14:41:58 +0500

    initial code commit

---
+++ config/dev.example.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com",
+  googleClientSecret: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  mongoURI:
+    "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  cookieKey: "123123123",
+};
